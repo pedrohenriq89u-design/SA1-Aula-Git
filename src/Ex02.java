0902f954a1c89739fa4de329a2aa3da638d3d1d1
pedrohenriq89u-design <pedrohenriq89u@gmail.com>
2026-03-18 16:11:18 -0300

    feat(Ex02): pedido.

--- src/Ex02.java
+++ src/Ex02.java
@@ -2,5 +2,25 @@ import java.util.Scanner;
 
 public class Ex02 {
 
-    Scanner sc = new Scanner(System.in);
+    static void main(){
+
+        Scanner sc = new Scanner(System.in);
+        double preco;
+        String nome;
+        String pedido;
+
+        IO.println("Qual seu nome?");
+        nome = sc.nextLine();
+        IO.println("Qual o prato desejado?");
+        pedido = sc.nextLine();
+        IO.println("Qual o valor do prato?");
+        preco = sc.nextDouble();
+        IO.println("As informações do seu pedido são:");
+        IO.println("Nome do cliente: " + nome);
+        IO.println("O pedido é: " + pedido);
+        IO.println("O valor é: " + preco);
+
+    }
+
+
 }
