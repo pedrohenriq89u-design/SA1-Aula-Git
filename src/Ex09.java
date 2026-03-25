2d1adb258cc0b8b686a47803e47daf9c4644c6d9
pedrohenriq89u-design <pedrohenriq89u@gmail.com>
2026-03-25 13:56:11 -0300

    Feat(ex09):frete

--- src/Ex09.java
+++ src/Ex09.java
@@ -3,7 +3,18 @@ import java.util.Scanner;
 public class Ex09 {
     static void main() {
         Scanner sc = new Scanner(System.in);
+        Double numero;
+        IO.println("Digite o valor da sua compra?");
+        numero = sc.nextDouble();
+
+        if (numero >= 100.00) {
+            IO.println("Vossa senhoria tem direito ao frete grátis ");
+        } else {
+            IO.println("Vossa senhoria não tem frete grátis adicione mais produtos ");
+            IO.println("Faltam: " );
+            IO.println(numero - 100.00);
 
 
+        }
     }
 }
