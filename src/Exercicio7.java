b0f90cea115d3f9dcd338cf21c9a545e5394aaf2
pedrohenriq89u-design <pedrohenriq89u@gmail.com>
2026-04-08 16:22:35 -0300

    feat(exercicio7)atividade7.

---
+++ src/Exercicio7.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Exercicio7 {
+
+    static void main() {
+
+        Scanner sc = new Scanner(System.in);
+
+        int numero = 0;
+
+        while (numero <= 50) {
+            IO.println(numero);
+            numero += 2;
+
+        }
+    }
+}
