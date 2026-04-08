32b87c8423a1d322b02e82b287f10eedae8b905b
pedrohenriq89u-design <pedrohenriq89u@gmail.com>
2026-04-08 16:03:35 -0300

    feat(exercicio6)atividade6.

---
+++ src/Exercicio6.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Exercicio6 {
+    static void main() {
+
+        Scanner sc = new Scanner(System.in);
+        int numero = 1;
+        while (numero != 24) {
+            IO.println("Digite um numero: ");
+         numero = sc.nextInt();
+
+
+        }
+    }
+
+}
