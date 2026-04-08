5b6f447a0cfaadb4a4c6550aef3d4b781cd63a2f
pedrohenriq89u-design <pedrohenriq89u@gmail.com>
2026-04-08 15:05:49 -0300

    feat(ex03):exercicio3.

--- src/Ex15.java
+++ src/Ex15.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Ex15 {
+    static void main() {
+
+        Scanner sc = new Scanner(System.in);
+
+        IO.println("Quantas calorias foram consumidas?");
+        double total = sc.nextDouble();
+
+        if (total <= 2000) {
+            IO.println("Está dentro do limite ");
+
+        } else if (total > 2000) {
+            IO.println("Você está fora do limite e ultrapassou: ");
+            IO.println(total - 2000);
+
+        }
+    }
 }
---
+++ src/Exercicio3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Exercicio3 {
+    static void main() {
+
+        Scanner sc = new Scanner(System.in);
+
+        int numero = 10;
+        while (numero >= 0) {
+            IO.println("" + numero);
+            numero--;
+        }
+    }
+}
